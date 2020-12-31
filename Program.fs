b6aced95fa86e72269d5b2b1f30ca0728b00dfc4
asti <asti@dynz.net>
2020-12-31 17:17:20 +0530

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,40 @@
+﻿module Program
+open FSharp.Text.Lexing
+open TexParsing
+open BenchmarkDotNet.Attributes
+
+let parse tex = 
+    let lexbuf = LexBuffer<char>.FromString tex
+    let res = Parser.start Lexer.tokenize lexbuf
+    res
+
+let lex tex =
+     LexBuffer<char>.FromString tex
+     |> List.unfold(fun buf -> 
+         if buf.IsPastEndOfStream then 
+             None 
+         else
+             let token = Lexer.tokenize buf
+             Some(token, buf)
+     ) 
+
+type TestBench () =    
+    let simpleTex = @"Hello World \\ \texbf"
+    
+    [<Benchmark>]
+    member _.ParseSimple () =
+        simpleTex |> parse <> TexBlock.Empty
+
+[<EntryPoint>]
+let main argv = 
+ 
+    let sampleTex = @"Hello World \\ \texbf \textit{So it} \$ \margin{1}{2}{3}{4}"
+    let lexResult = sampleTex |> lex
+    printfn "Lex:\n%A" lexResult
+    let parseResult = sampleTex |> parse
+    printfn "Parse:\n%A" parseResult
+    
+    #if RELEASE
+    BenchmarkDotNet.Running.BenchmarkRunner.Run<TestBench>() |> ignore
+    #endif
+    0
---
+++ TexParsing.fs
@@ -0,0 +1,23 @@
+﻿module TexParsing
+
+open System.Drawing
+
+type Style =
+    | Normal = 0
+    | Bold = 1
+    | Italic = 2
+
+[<RequireQualifiedAccess>]
+type FE =
+    | Word of string * Style * Color
+    | Space
+    | Math of string * Color  
+    
+type TexBlock =
+    | Items of TexBlock list
+    | Group of string * TexBlock list
+    | Command of id:string
+    | CommandVariadic of id:string * TexBlock list list
+    | Text of string
+    | Break
+    | Empty
